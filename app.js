8bf2210404b1413a9d40055ac40067e5253a081b
SrinivasaKamathR <kamathsri1997@gmail.com>
2023-03-08 13:27:37 +0530

    body-parser-middleware and routing

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("I am in Middle");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/add-product", (req, res, next) => {
+  console.log("I am in  another Middle add");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>'
+  );
 });
-app.use((req, res, next) => {
-  console.log("I am in  another Middle");
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
   res.send("<h1>Hello Express</h1>");
 });
-
-// const server = http.createServer(app);
-
-// server.listen(4000);
 
 app.listen(4000);
