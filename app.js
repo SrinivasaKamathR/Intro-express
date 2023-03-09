7b7bd2e9b2d96b56aca0f3b89435c540909a9231
SrinivasaKamathR <kamathsri1997@gmail.com>
2023-03-09 11:56:36 +0530

    routing-filter

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
 const express = require("express");
+
 const bodyParser = require("body-parser");
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/add-product", (req, res, next) => {
-  console.log("I am in  another Middle add");
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>'
-  );
-});
 
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
+app.use("/admin", adminRoutes);
+app.use("/shop", shopRoutes);
 
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello Express</h1>");
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not fount</h1>");
 });
 
 app.listen(4000);
